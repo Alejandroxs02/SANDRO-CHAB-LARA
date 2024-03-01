5e49e0eae85144b8752f0b1acb5f437d98ce3fb2
Alejandroxs02 <8429@itescam.edu.mx>
2024-03-01 15:27:25 -0600

    Cambi de nombre archivo

--- Actividad.java
+++ Actividad.java
@@ -1,4 +1,4 @@
-public class Actividad1{
+xpublic class Actividad1{
 	public static void main(String []args){
 	System.out.println("Ariel Alejandro Garcia Huchin");
 	System.out.println("Jose Manuel Morales Soriano");
