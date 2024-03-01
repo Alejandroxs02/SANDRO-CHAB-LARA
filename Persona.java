797d804b35cab80cedb4f367badb6b27bb6760ec
Alejandroxs02 <8429@itescam.edu.mx>
2024-03-01 16:10:38 -0600

    modificacion del codigo

--- Persona.java
+++ Persona.java
@@ -2,16 +2,12 @@ public class Persona{
 	String nombre;
 	int matricula;
 
-	public void setNombre(String nombre){
-	this.nombre = nombre
+	public Persona(String nombre, int matricula){
+	this.nombre = nombre;
+	this.matricula = matricula;
 	}
 
-	public String getNombre
-
-	public String nombrar(String nombre){
-	setNombre()
-	}
-	public static void main(String []args){
-	
+	public void imprimir(){
+	System.out.println("nombre: " + nombre + +""+"matricula:" + matricula);
 	}
 }
