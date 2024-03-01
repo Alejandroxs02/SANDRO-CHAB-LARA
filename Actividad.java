74afbea658e59d99e5b6f4b3a85bd2884d287c9a
SandroRafaelChi <8413@itescam.edu.mx>
2024-03-01 15:35:19 -0600

    Modificacion del archivo Java

--- Actividad.java
+++ Actividad.java
@@ -2,5 +2,7 @@ xpublic class Actividad{
 	public static void main(String []args){
 	System.out.println("Ariel Alejandro Garcia Huchin");
 	System.out.println("Jose Manuel Morales Soriano");
+        System.out.println("Sandro Rafael Chi Uc");	
+	
 	}
 }
