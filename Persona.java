4b039348960794f84c530ef4bff10c8aced16448
Alejandroxs02 <8429@itescam.edu.mx>
2024-03-01 15:52:47 -0600

    nueva rama y documento

---
+++ Persona.java
@@ -0,0 +1,17 @@
+public class Persona{
+	String nombre;
+	int matricula;
+
+	public void setNombre(String nombre){
+	this.nombre = nombre
+	}
+
+	public String getNombre
+
+	public String nombrar(String nombre){
+	setNombre()
+	}
+	public static void main(String []args){
+	
+	}
+}
