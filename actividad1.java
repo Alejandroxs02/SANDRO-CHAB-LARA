876620c5b1f674a5000831fb32286ba0654353e5
JoseMorales8426 <8426@itescam.edu.mx>
2024-03-01 15:19:59 -0600

    Puse el metodo para imprimir mi nombre

--- actividad1.java
+++ actividad1.java
@@ -1,5 +1,6 @@
 public class Actividad1{
 	public static void main(String []args){
-	system.out.println("Ariel Alejandro Garcia Huchin");
+	System.out.println("Ariel Alejandro Garcia Huchin");
+	System.out.println("Jose Manuel Morales Soriano");
 	}
 }
